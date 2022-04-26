c8be25807fa4867d27a91d78c60542fe9f96d57a
littlelerroyy <raptorial_radiation@live.com.au>
2022-04-26 18:11:23 +1000

    Import bootstrap javascript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./App.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap";
+import "./App.scss";
 
 ReactDOM.render(
   <React.StrictMode>
